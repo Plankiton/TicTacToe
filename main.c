7c9c94847fc49502543e6888e5ac925ca4bae221
RoboCopGay <vinicios.sousa909@gmail.com>
2019-11-09 17:47:07 -0300

    clearing tree...

--- main.c
+++ main.c
@@ -1,35 +1,29 @@
 #include "gui/gui.c"
 
 void doWin(Position winner){
-    putchar(winner?'X':'O');
-    puts(" é o vencedor!");
+    gtk_main_quit();
 }
 
 int main(int argc, char *argv[])
 {
 
-    int playRound = 0;
     ArgData args[3][3];
 
-    TicTacToe tictactoe[3][3];
+    TicTacToe ttt;
+    Boxes * boxes = getBoxes(&ttt);
+    Board * board =  getBoard(&ttt);
+    board->whenWin = doWin;
+    initBoard(board);
+
+    int playRound = 0;
 
     initGUI();
 
+    GtkWidget * container = createBoardGui(&ttt);
     GtkWidget * window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    GtkWidget * container[3][3];
-    GtkWidget * root_container;
-    GtkWidget * hcontainer[3];
+    setButtonsSettings(args, boxes, board, playRound);
 
-    root_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    for (int c = 0; c<3; c++){
-        hcontainer[c] = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-        gtk_box_pack_start(GTK_BOX(root_container), hcontainer[c], TRUE, TRUE, 0);
-        for (int b = 0; b<3; b++){
-            container[c][b] = createBoardGui(&tictactoe[c][b]);
-            gtk_box_pack_start(GTK_BOX(hcontainer[c]), container[c][b], TRUE, TRUE, 10);
-        }
-    }
+    endGUI(container, window);
 
-    endGUI(root_container, window);
     return 0;
 }
