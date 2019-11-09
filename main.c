c3e15b84fa2ef8426c17ddb15886fffd4c598066
RoboCopGay <vinicios.sousa909@gmail.com>
2019-11-09 18:34:10 -0300

    finalizate...

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include "gui/gui.c"
 
-void doWin(Position winner){
-    gtk_main_quit();
+void doWin(Position player){
+    putchar(player?'X':'O');
+    puts(" is the winner!!");
+
+    char * winner = player?"X is the winner!!":"O is the winner!!";
+
+    GtkWidget * win_popup = gtk_window_new(GTK_WINDOW_POPUP);
+
+    GtkWidget * box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_widget_set_size_request(box, 300, 100);
+
+    GtkWidget * label = gtk_label_new(winner);
+    GtkWidget * button = gtk_button_new_with_label("Quit");
+    g_signal_connect(button, "clicked",
+             G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_box_pack_start(GTK_BOX(box), label, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(box), button, TRUE, TRUE, 0);
+
+    gtk_window_set_default_size(GTK_WINDOW(win_popup), 400, 200);
+    gtk_container_add(GTK_CONTAINER(win_popup), box);
+    gtk_window_set_position (GTK_WINDOW (win_popup),GTK_WIN_POS_CENTER);
+    gtk_widget_show_all(win_popup);
+    g_signal_connect(win_popup, "delete_event",
+             G_CALLBACK(gtk_main_quit), NULL);
 }
 
 int main(int argc, char *argv[])
---
+++ readme.md
@@ -0,0 +1,5 @@
+# TicTacToe
+
+It is just a simple C project made as the object of study, because of this it is very simple and probably wrong in the most professional eyes.
+
+É apenas um projeto em C simples feito como o objeto de estudo, por causa disso, é muito simples e provavelmente errado à olhos mais proficionais.
